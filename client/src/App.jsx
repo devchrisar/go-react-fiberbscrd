ce777223f4f652c23b66f1233d5f822ce27885ae
Devchrisar <christopher.ariasar@amigo.edu.co>
2023-04-03 20:29:00 -0500

    feat: Added get and post method

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,43 @@
+import {useEffect, useState} from "react";
+
 function App() {
-  return (
-    <div>
-      <h1>Hello Vite + React! con GO</h1>
-      <button
-        onClick={async () => {
-          const response = await fetch("/users");
-          const data = await response.json();
-          console.log(data);
-        }}
-      >
-        Obtener datos
-      </button>
-    </div>
-  );
+    const [name, setName] = useState('')
+    const [users, setUsers] = useState([])
+
+    async function loadUsers() {
+        const response = await fetch('/users')
+        const data = await response.json()
+        setUsers(data.users)
+    }
+    useEffect(() => {
+        loadUsers()
+    }, [])
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await fetch('/users', {
+            method: 'POST',
+            body: JSON.stringify({name}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        loadUsers()
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="coloca tu nombre"
+                       onChange={(e) => setName(e.target.value)}/>
+                <button>Guardar</button>
+            </form>
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/devchrisar/go_reactBasicCrud/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
 )
 
@@ -13,11 +18,41 @@ func main() {
 		port = "3000"
 	}
 	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodb"))
+	if err != nil {
+		panic(err)
+	}
 	app.Use(cors.New())
 	app.Static("/", "client/dist")
-	app.Get("/users", func(c *fiber.Ctx) error {
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		var user models.User
+		c.BodyParser(&user)
+		coll := client.Database("gomongodb").Collection("users")
+		result, err := coll.InsertOne(context.TODO(), bson.D{
+			{"name", user.Name},
+		})
+		if err != nil {
+			panic(err)
+		}
 		return c.JSON(&fiber.Map{
-			"data": "usuarios desde el backend",
+			"data": result,
+		})
+	})
+	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []models.User
+		coll := client.Database("gomongodb").Collection("users")
+		results, err := coll.Find(context.TODO(), bson.M{})
+		if err != nil {
+			panic(err)
+		}
+		for results.Next(context.TODO()) {
+			var user models.User
+			results.Decode(&user)
+			users = append(users, user)
+		}
+		return c.JSON(&fiber.Map{
+			"users": users,
 		})
 	})
 	app.Listen(":" + port)
